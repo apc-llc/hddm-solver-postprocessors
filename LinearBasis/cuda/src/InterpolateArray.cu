29dec516bcc570089ee1bf15d9049cafeafecdb2
Dmitry Mikushin <dmitry@kernelgen.org>
2016-09-04 20:18:58 +0200

    Correcting InterpolateArray, similar to earlier fixes to multistate version

--- LinearBasis/cuda/src/InterpolateArray.cu
+++ LinearBasis/cuda/src/InterpolateArray.cu
@@ -25,11 +25,20 @@ inline __attribute__((always_inline)) __device__ double atomicAdd(double* addres
 }
 #endif // __CUDA_ARCH__
 
+inline __attribute__((always_inline)) __device__ double warpReduceMultiply(double val)
+{
+	for (int offset = warpSize / 2; offset > 0; offset /= 2)
+		val *= __shfl_down(val, offset);
+	return val;
+}
+
 static __global__ void InterpolateArray_kernel_large_dim(
 	const int dim, const int vdim, const int nno,
 	const int Dof_choice_start, const int Dof_choice_end, const double* x,
 	const Matrix<int>::Device* index_, const Matrix<double>::Device* surplus_, double* value)
 {
+	extern __shared__ double temps[];
+
 	const Matrix<int>::Device& index = *index_;
 	const Matrix<double>::Device& surplus = *surplus_;
 
@@ -49,19 +58,37 @@ static __global__ void InterpolateArray_kernel_large_dim(
 		temp *= max(0.0, xp);
 	}
 	
+	// Multiply all partial temps within a warp.
+	temp = warpReduceMultiply(temp);
+	
+	// Gather temps from all participating warps corresponding to the single DIM
+	// into a shared memory array.
+	int lane = threadIdx.x % warpSize;
+	int warpId = threadIdx.x / warpSize;
+	int nwarps = blockDim.x / warpSize;
+	if (lane == 0)
+		temps[warpId + threadIdx.y * nwarps] = temp;
+
+	// Wait for all partial reductions.
 	__syncthreads();
 
-	// Multiply all partial temps within a warp.
-	for (int offset = warpSize / 2; offset > 0; offset /= 2)
-		temp *= __shfl_down(temp, offset);
-	
-	// We can only exit at this point, where all threads are synchronized.
+	// We can only exit at this point, when all threads in block are synchronized.
 	if (!temp) return;
 	if (i >= nno) return;
+	
+	// Read from shared memory only if that warp existed.
+	temp = (threadIdx.x < blockDim.x / warpSize) ? temps[lane + threadIdx.y * nwarps] : 1.0;
+
+	// Final reduction within the first warp.
+	if (warpId != 0) return;
+	temp = warpReduceMultiply(temp);
+	
+	if (threadIdx.x != 0) return;
 
 	// Atomically add to the output value.
 	// Uses double precision atomicAdd code above, since all
 	// generations of GPUs before Pascal do not have double atomicAdd builtin.
+	// TODO: precompute the sum of surplus rows
 	for (int j = Dof_choice_start; j <= Dof_choice_end; j++)
 		atomicAdd(&value[j - Dof_choice_start], temp * surplus(i, j));
 }
@@ -126,22 +153,27 @@ extern "C" void FUNCNAME(
 		if (nno % blockDim.x)
 			gridDim.x++;
 	}
-	
-	real* dx;
-	CUDA_ERR_CHECK(cudaMalloc(&dx, sizeof(real) * DIM));
+
+	// Calculate the number of warps in block.
+	// It shall denote the size of shared memory used for
+	// inter-warp step of temp value reduction.
+	int nwarps = (blockDim.x * blockDim.y) / device->warpSize;
+
+	double* dx;
+	CUDA_ERR_CHECK(cudaMalloc(&dx, sizeof(double) * DIM));
 
 	const int length = Dof_choice_end - Dof_choice_start + 1;
-	real* dvalue;
-	CUDA_ERR_CHECK(cudaMalloc(&dvalue, sizeof(real) * length));
+	double* dvalue;
+	CUDA_ERR_CHECK(cudaMalloc(&dvalue, sizeof(double) * length));
 	
 	cudaStream_t stream;
 	CUDA_ERR_CHECK(cudaStreamCreate(&stream));
-	CUDA_ERR_CHECK(cudaMemcpyAsync(dx, x, sizeof(real) * DIM, cudaMemcpyHostToDevice, stream));
-	CUDA_ERR_CHECK(cudaMemsetAsync(dvalue, 0, sizeof(real) * length, stream));
-	InterpolateArray_kernel_large_dim<<<gridDim, blockDim, 0, stream>>>(
+	CUDA_ERR_CHECK(cudaMemcpyAsync(dx, x, sizeof(double) * DIM, cudaMemcpyHostToDevice, stream));
+	CUDA_ERR_CHECK(cudaMemsetAsync(dvalue, 0, sizeof(double) * length, stream));
+	InterpolateArray_kernel_large_dim<<<gridDim, blockDim, nwarps, stream>>>(
 		dim, vdim, nno, Dof_choice_start, Dof_choice_end, dx, index, surplus, dvalue);
 	CUDA_ERR_CHECK(cudaGetLastError());
-	CUDA_ERR_CHECK(cudaMemcpyAsync(value, dvalue, sizeof(real) * length, cudaMemcpyDeviceToHost, stream));
+	CUDA_ERR_CHECK(cudaMemcpyAsync(value, dvalue, sizeof(double) * length, cudaMemcpyDeviceToHost, stream));
 	CUDA_ERR_CHECK(cudaStreamSynchronize(stream));
 	CUDA_ERR_CHECK(cudaStreamDestroy(stream));
 
